78fc441cfdeee6efd7b293c60de37c295d2175d1
Josh Romea <josh.romea@gmail.com>
2019-02-09 17:21:48 -0700

    jawsdb added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const conn = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "starr801",
-    database: "burgers_db"
-});
+let conn;
+
+if (process.env.JAWSDB_URL) {
+    conn = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "starr801",
+        database: "burgers_db"
+    });
+}
 
 conn.connect((err) => {
     if (err) {
